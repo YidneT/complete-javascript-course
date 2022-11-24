f2bb4f3b3fc1c20af0fc138a72ee2bee12210aa9
YidneT <yidnektame@gmail.com>
2022-11-24 11:00:42 +0300

    SC PC 24-11-2022

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,6 +61,149 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+// Prepare and populate username for all users
+const populateUsername = function (accounts) {
+  accounts.forEach(account => {
+    account.username = account.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+}
+populateUsername(accounts);
+
+const clearInput = function(input){
+  if (Array.isArray(input)) {
+    input.forEach(element => {
+      element.value = '';
+    });
+  } else {
+    element.value = '';
+  }
+}
+
+// Login Users
+let loggedInUser = account2;
+
+const authUser = function (user, pin) {
+  return accounts.find(account => account.username === user && account.pin === Number(pin));  
+}
+
+const loginUser = function (account) {
+  displayAccountDetails(account);
+  populateHeader(account);
+  containerApp.style.opacity = 1;
+  console.log(accounts);
+}
+
+const populateHeader = function (account) {
+  labelWelcome.innerHTML = `Howdy ${account.owner}`;
+  clearInput([inputLoginUsername, inputLoginPin]);
+  /* document.querySelector('nav').style.display = 'none';
+  const greetings = `
+    <div style=" display: flex; justify-content: space-between; width: 100%; ">
+      <h2>Howdy ${account.owner}</h2>
+      <button class="login__btn" style=" font-size: 150%; ">Logout 👋</button>
+    </div>
+  `;
+  document.querySelector('body').insertAdjacentHTML("afterbegin", greetings); */
+}
+
+const populateMovements = function (account) {
+  const movements = account.movements;
+  containerMovements.innerHTML = '';
+  movements.forEach((move, index) => {
+    const type = move > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${index + 1} ${type.toUpperCase()}</div>
+        <div class="movements__date">${Math.floor(Math.random() * 30) } days ago</div>
+        <div class="movements__value">$ ${move}</div>
+      </div>
+    `;
+    containerMovements.insertAdjacentHTML("afterbegin", html);    
+  });
+
+}
+
+const populateSummary = function (account) {
+  const movements = account.movements;
+  const balance = movements
+    .reduce((acc, move) => acc + move, 0);
+  labelBalance.innerHTML = balance;
+  account.balance = balance;
+  const deposit = movements
+    .filter(move => move > 0)
+    .reduce((acc, move) => acc + move, 0);
+  labelSumIn.innerHTML = deposit;
+  account.deposit = deposit;
+  const withdrawal = Math.abs(
+    movements
+    .filter(move => move < 0)
+    .reduce((acc, move) => acc + move, 0)
+  );
+  labelSumOut.innerHTML = withdrawal;
+  account.withdrawal = withdrawal;
+  const interest = movements
+    .filter(move => move > 0)
+    .map(move => move * account.interestRate/100)
+    .reduce((acc, move) => acc + move, 0);
+  labelSumInterest.innerHTML   = interest;
+  account.interest = interest;
+}
+
+const displayAccountDetails = function (account) {
+  populateMovements(account);
+  populateSummary(account);
+}
+
+const submitLogin = function (e) {
+  const user = inputLoginUsername.value;
+  const pin = inputLoginPin.value;
+  loggedInUser = authUser(user, pin);
+  if (loggedInUser) {
+    loginUser(loggedInUser);
+    console.log(loggedInUser);    
+  } else {
+    inputLoginUsername.style.borderColor = 'red';
+    inputLoginPin.style.borderColor = 'red';
+    console.log('Incorect details');    
+  }
+  e.preventDefault();
+}
+btnLogin.addEventListener('click', submitLogin);
+
+const transferPayment = function (e) {
+  const transferTo = inputTransferTo.value;
+  const transferAmount = Number(inputTransferAmount.value);
+  const accTo = accounts.find(acc => acc.username === transferTo);
+
+  if(transferTo !== loggedInUser.username && accTo !== undefined){
+    if (transferAmount > 0 && transferAmount < loggedInUser.balance) {
+      loggedInUser.movements.push(transferAmount * -1);
+      accTo.movements.push(transferAmount);
+      displayAccountDetails(loggedInUser);
+      clearInput([inputTransferTo, inputTransferAmount]);
+      document.querySelector('.operation--transfer').insertAdjacentHTML("beforeend", `<p style="color: green">Submited $ ${transferAmount} to ${accTo.owner} succesfuly</p>`);
+      console.log(accounts);
+    } else {
+      document.querySelector('.operation--transfer').insertAdjacentHTML("beforeend", `<p style="color: red">Transfer amount error!</p>`);
+    }
+  } else{
+    this.style.backgroundColor = 'red';
+    console.log('Incorect details');
+  }
+  // console.log(transferTo, transferAmount);
+  e.preventDefault();
+}
+btnTransfer.addEventListener('click', transferPayment);
+
+/* Temporary */
+loginUser(loggedInUser);
+/* Temporary */
+
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
