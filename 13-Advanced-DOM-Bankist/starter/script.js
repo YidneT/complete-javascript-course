d933d3d81078d120e00e20451d4beda357d53a2c
YidneT <yidnektame@gmail.com>
2022-12-27 19:47:06 +0300

    SC PC 12-27-2022

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -29,3 +29,45 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  // Old school way
+  /* const s1Coords = section1.getBoundingClientRect();
+  console.log(s1Coords);
+
+  window.scrollTo({
+    left: s1Coords.left + window.pageXOffset, 
+    top: s1Coords.top + window.pageYOffset,
+    behavior: 'smooth'
+  }); */
+
+  section1.scrollIntoView({behavior: 'smooth'});
+});
+
+
+// ## Types of event handlers
+/* 
+const h1  = document.querySelector('h1');
+
+h1.onmouseenter = function (e) {
+  console.log('Clicked H1');
+}
+
+h1.addEventListener('click', function (e) {
+  console.log('Clicke H1: addEventListner');
+})
+
+const alertH1 = function (e) {
+  alert('Happiness');
+  h1.removeEventListener('mouseenter', alertH1)
+}
+h1.addEventListener('mouseenter', alertH1); */
+
+// ## Event propagation Bubbling and capturing
+
+
+
