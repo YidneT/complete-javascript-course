2ea7db9a80cf78d0fbb518487430ca123a3f8be8
YidneT <yidnektame@gmail.com>
2023-01-04 08:21:51 +0300

    Ch 13 Complete

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -1,12 +1,22 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+const header = document.querySelector('.header');
+const nav = document.querySelector('.nav');
+
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slide');
+const dotContainer = document.querySelector('.dots');
+
+///////////////////////////////////////
+// Modal window
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -30,9 +40,8 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+///////////////////////////////////////
+// Scroll to features
 
 btnScrollTo.addEventListener('click', function (e) {
   // Old school way
@@ -45,9 +54,124 @@ btnScrollTo.addEventListener('click', function (e) {
     behavior: 'smooth'
   }); */
 
-  section1.scrollIntoView({behavior: 'smooth'});
+  section1.scrollIntoView({ behavior: 'smooth' });
 });
 
+///////////////////////////////////////
+// Sticky Navigation
+
+/* const featureCord = section1.getBoundingClientRect().top;
+window.addEventListener('scroll', function (e) {
+  if (window.scrollY > featureCord) {
+    nav.classList.add('sticky');
+  } else {
+    nav.classList.remove('sticky');    
+  }
+}); */
+
+const navHeight = nav.getBoundingClientRect().height;
+const stickyNav = function (entries, observer) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      nav.classList.add('sticky');
+    } else {
+      nav.classList.remove('sticky');
+    }
+  });
+};
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  rootMargin: `-${navHeight}px`,
+  threshold: 0,
+});
+headerObserver.observe(header);
+
+///////////////////////////////////////
+// Reaveal Elements on Scroll
+const allSections = document.querySelectorAll('section');
+const secDisplay = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(secDisplay, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(section => {
+  // section.classList.add('section--hidden');
+  sectionObserver.observe(section);
+});
+
+///////////////////////////////////////
+// Lazy Loading Images
+const allImages = document.querySelectorAll('.features__img');
+const imgDisplay = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  const target = entry.target;
+  target.src = target.dataset.src;
+  target.classList.remove('lazy-img');
+  observer.unobserve(entry.target);
+};
+const imgObserver = new IntersectionObserver(imgDisplay, {
+  root: null,
+  rootMargin: '100px',
+  threshold: 0.15,
+});
+allImages.forEach(img => {
+  imgObserver.observe(img);
+});
+
+///////////////////////////////////////
+// Slider Component
+const slideMe = function (curSlide) {
+  slides.forEach((slide, i) => {
+    slide.style.transform = `translateX(${100 * (i - curSlide)}%)`;
+  });
+};
+let currentSlide = 0;
+slideMe(currentSlide);
+
+slider.addEventListener('click', function (e) {
+  if (e.target.classList.contains('slider__btn--left')) {
+    // Previous Slide
+    if (currentSlide > 0) {
+      currentSlide--;
+      slideMe(currentSlide);
+    } else {
+      currentSlide = slides.length - 1;
+      slideMe(currentSlide);
+    }
+  } else if (e.target.classList.contains('slider__btn--right')) {
+    // Next Slide
+    if (currentSlide < slides.length - 1) {
+      currentSlide++;
+      slideMe(currentSlide);
+    } else {
+      currentSlide = 0;
+      slideMe(currentSlide);
+    }
+  }
+});
+
+// Create dot slider
+const createDots = function () {
+  slides.forEach((slide, i) => {
+    const button = document.createElement('button');
+    button.classList.add('dots__dot');
+    button.dataset.slide = i;
+    dotContainer.insertAdjacentElement('beforeend', button);
+  });
+};
+createDots();
+dotContainer.addEventListener('click', function (e) {
+  if (e.target.classList.contains('dots__dot')) {
+    currentSlide = e.target.dataset.slide;
+    slideMe(currentSlide);
+  }
+});
 
 // ## Types of event handlers
 /* 
@@ -68,6 +192,3 @@ const alertH1 = function (e) {
 h1.addEventListener('mouseenter', alertH1); */
 
 // ## Event propagation Bubbling and capturing
-
-
-
