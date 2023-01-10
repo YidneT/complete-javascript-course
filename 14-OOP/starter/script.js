11ba687e3c05c7229e8df27c437daae95db04f30
YidneT <yidnektame@gmail.com>
2023-01-10 14:05:33 +0300

    SC PC ch-14 start 10-01-23

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+// ## COnstructor funtions and the new operator
+const Person = function(fName, bYear) {
+  this.fName = fName;
+  this.bYear = bYear;
+
+};
+
+Person.prototype.calAge = function () {
+  return Number((new Date()).getFullYear()) -  this.bYear;
+}
+
+const p1 = new Person('Abebe', 1990);
+const p2 = new Person('Eden', 1996);
+console.log(p1, p2);
+console.log(p1.calAge());
+console.log(p2.__proto__);
